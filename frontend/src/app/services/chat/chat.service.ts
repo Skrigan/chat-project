72b70dc26de0021f0401abdcd2b2314f1ec5b0ee
Skrigan <sk_dm-821@mail.ru>
2023-03-05 16:11:51 +0300

    fix: submodule issues

---
+++ frontend/src/app/services/chat/chat.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {io, Socket } from 'socket.io-client';
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  private socket: Socket;
+  private url = 'http://localhost:3000';
+  // private url = '' //global link
+
+  constructor() {
+    this.socket = io(this.url, {transports: ['websocket', 'polling', 'flashsocket']});
+  }
+
+  joinRoom(data: any): void {
+    this.socket.emit('join', data);
+  }
+
+  sendMessage(data: any): void {
+    this.socket.emit('message', data);
+  }
+
+  getMessage(): Observable<any> {
+    return new Observable<{user: string, message: string}>(observer => {
+      this.socket.on('new message', (data) => {
+        observer.next(data);
+      });
+
+      return () => {
+        this.socket.disconnect();
+      }
+    });
+  }
+
+  getStorage() {
+    const storage: any = localStorage.getItem('chats');
+    return storage ? JSON.parse(storage) : [];
+  }
+
+  setStorage(data) {
+    localStorage.setItem('chats', JSON.stringify(data));
+  }
+
+ }
